1917c92e70bbf0b121c228659402f36c54225c14
Truong Quang Tuan <tuan.truong@kt-techno.com>
2019-04-05 18:33:53 +0700

    Just for test

---
+++ TTBaseUIKit/TTBaseUIKit/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  TTBaseUIKit
+//
+//  Created by Truong Quang Tuan on 4/5/19.
+//  Copyright © 2019 Truong Quang Tuan. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    private init() {}
+    
+    public func printData() {
+        print("Hello Service")
+    }
+}
---
+++ TTBaseUIKit/TTBaseUIKit/TTBaseUIKit.h
@@ -0,0 +1,19 @@
+//
+//  TTBaseUIKit.h
+//  TTBaseUIKit
+//
+//  Created by Truong Quang Tuan on 4/5/19.
+//  Copyright © 2019 Truong Quang Tuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TTBaseUIKit.
+FOUNDATION_EXPORT double TTBaseUIKitVersionNumber;
+
+//! Project version string for TTBaseUIKit.
+FOUNDATION_EXPORT const unsigned char TTBaseUIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TTBaseUIKit/PublicHeader.h>
+
+
