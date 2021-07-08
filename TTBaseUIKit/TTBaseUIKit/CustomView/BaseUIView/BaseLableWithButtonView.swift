8dd8ab5a68c4ec1bc153cfd36fde9ae26e765393
Tuan.Truong <truongquangtuanit@gmail.com>
2021-07-08 11:17:39 +0700

    improve UI/UX, add validation text

---
+++ TTBaseUIKit/TTBaseUIKit/CustomView/BaseUIView/BaseLableWithButtonView.swift
@@ -0,0 +1,9 @@
+//
+//  BaseLableWithButtonView.swift
+//  TTBaseUIKit
+//
+//  Created by Tuan Truong Quang on 7/8/21.
+//  Copyright © 2021 Truong Quang Tuan. All rights reserved.
+//
+
+import Foundation
