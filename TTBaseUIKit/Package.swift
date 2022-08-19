2d808c143d00a8ebbf259e92714f5e3c66cca1cb
Tuan.Truong <truongquangtuanit@gmail.com>
2022-08-19 17:03:25 +0700

    feat(swiftPM): limit platforms only iOS

--- TTBaseUIKit/Package.swift
+++ TTBaseUIKit/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TTBaseUIKit",
     platforms: [
-            .iOS(.v10),.macCatalyst(.v13)
+            .iOS(.v10)
        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
