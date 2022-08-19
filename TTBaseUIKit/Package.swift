bce7997277f0e7691c2effabb864ef245ce69942
Tuan.Truong <truongquangtuanit@gmail.com>
2022-08-19 15:57:46 +0700

    refator(cocoapod): change path

--- TTBaseUIKit/Package.swift
+++ TTBaseUIKit/Package.swift
@@ -20,9 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TTBaseUIKit",
-            dependencies: []),
-        .testTarget(
-            name: "TTBaseUIKitTests",
-            dependencies: ["TTBaseUIKit"]),
+            path: "Sources"
+        )
     ]
 )
