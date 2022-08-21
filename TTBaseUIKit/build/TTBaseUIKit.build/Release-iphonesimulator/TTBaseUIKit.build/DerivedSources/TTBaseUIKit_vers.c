54c414ec9a1d37c9a32cee8775e1182eda590d9d
Tuan.Truong <truongquangtuanit@gmail.com>
2022-08-21 14:51:05 +0700

    - clear

--- TTBaseUIKit/build/TTBaseUIKit.build/Release-iphonesimulator/TTBaseUIKit.build/DerivedSources/TTBaseUIKit_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char TTBaseUIKitVersionString[];
- extern const double TTBaseUIKitVersionNumber;
-
- const unsigned char TTBaseUIKitVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:TTBaseUIKit  PROJECT:TTBaseUIKit-1" "\n";
- const double TTBaseUIKitVersionNumber __attribute__ ((used)) = (double)1.;
--- TTBaseUIKit/build/TTBaseUIKit.build/Release-iphonesimulator/TTBaseUIKit.build/Objects-normal/i386/TTBaseUIKit_vers.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/Shared/STORE/S.JOB/J.PROJECT/P.BUILD_LIB/TTBaseUIKit/TTBaseUIKit/build/TTBaseUIKit.build/Release-iphonesimulator/TTBaseUIKit.build/DerivedSources/TTBaseUIKit_vers.c
